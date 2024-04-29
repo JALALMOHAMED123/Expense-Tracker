2e385ea8c5abc4a995f2e784ae193388816875ac
Jalal Mohamed <jalalmohamed2702@gmail.com>
2024-04-29 20:22:49 +0530

    Initial commit

---
+++ Expense_Tracker.js
@@ -0,0 +1,37 @@
+const form=document.querySelector('form');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const expensement=event.target.expense.value;
+    const desc=event.target.desc.value;
+    const category=event.target.category.value;
+    const obj={expensement,desc,category};
+
+    localStorage.setItem(expensement,JSON.stringify(obj));
+    showDetails(expensement, desc, category);
+    
+});
+function showDetails(expensement, desc, category){
+    const parent=document.querySelector('ul');
+    const li=document.createElement('li');
+
+    li.innerHTML=`${expensement} - ${desc} - ${category} <button type='button' id='Delete'>Delete</button> <button type='button' id='edit'>Edit</button>`;
+    parent.appendChild(li);
+
+    const deletebutton=li.querySelector('#Delete');
+    deletebutton.onclick = () => {
+        parent.removeChild(li);
+        localStorage.removeItem(expensement);
+    }
+
+    const editbutton=li.querySelector('#edit');
+    editbutton.onclick = () => {
+        document.getElementById('expensement').value=expensement;
+        document.getElementById('desc').value=desc;
+        document.getElementById('category').value=category;
+
+        parent.removeChild(li);
+        localStorage.removeItem(expensement);
+    }
+    
+}
